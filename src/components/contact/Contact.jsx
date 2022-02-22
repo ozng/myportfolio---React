50e9aac3e60f40bf4e1ca30b371f31efc08949db
Ozan Gürer <ozangurer12@gmail.com>
2022-02-23 01:30:54 +0300

    Contact bitti

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,6 @@
 import "./contact.scss";
 import { useState } from "react";
+import { Person, Mail } from "@material-ui/icons";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
@@ -16,6 +17,19 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact</h2>
+        <div className="emailPhoneContainer">
+          <a
+            href="mailto:ozangurer12@gmail.com?subject=Merhaba"
+            className="email"
+          >
+            <Mail className="mailIcon" />
+            <span>ozangurer12@gmail.com</span>
+          </a>
+          <div className="phone">
+            <Person className="phoneIcon" />
+            <span>+90 (530) 551 43 20</span>
+          </div>
+        </div>
         <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Message"></textarea>
